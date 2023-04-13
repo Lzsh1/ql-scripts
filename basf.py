8e1b981079f5d86cd2c26ecc228cbf86d37c1a38
ZENG61 <74004927+ZENG61@users.noreply.github.com>
2023-04-13 13:58:46 +0800

    Update basf.py

--- basf.py
+++ basf.py
@@ -1,27 +1,34 @@
 import requests
 
-url = "https://gc-eassistance.basf.com/api/gear-campus-quiz/daily-exam/start"
+url = 'https://gc-eassistance.basf.com/api/gear-campus-quiz/daily-exam/start'
 headers = {
-    "Cookie": "",
-    "User-Agent": "",
+    'User-Agent': '',
+    'Cookie': ''
 }
 
-response = requests.get(url, headers=headers)
-question_ids = [item["id"] for item in response.json()["data"]["questions"]]
+response = requests.post(url, headers=headers)##post考试的url
+data = response.json()##将响应主体放到data
 
-answers = {}  # 存储答案
+if "questions" not in data:##如果考试url中不存在questions则代表考试已完成，有则获取考试的问题id
+    print("今日考试已完成")
+else:
+    id = data['questions']['id'][0]
+    answers = {}
+##将考试的问题id到每日学习的url进行查询答案，再输出答案
+    for question_id in question_ids:
+        data = {
+            "id": str(question_id),
+            "userAnswer": [1]
+        }
 
-for question_id in question_ids:
-    data = {
-        "id": str(question_id),
-        "userAnswer": [1]
-    }
+        response = requests.post("https://gc-eassistance.basf.com/api/gear-campus-quiz/learning/answer", headers=headers, json=data)
+        answer = response.json()["data"]["answer"][0]
+        answers[str(question_id)] = answer
 
-    response = requests.post("https://gc-eassistance.basf.com/api/gear-campus-quiz/learning/answer", headers=headers, json=data)
-    answer = response.json()["data"]["answer"][0]
+    print("问题的正确答案如下：")
 
-    answers[str(question_id)] = answer
+    for question_id, answer in answers.items():
+        print(f"问题 '{question_id}' 的正确答案是: {answer}")
 
-print("问题的正确答案如下：")
-for question_id, answer in answers.items():
-    print(f"问题 '{question_id}' 的正确答案是: {answer}")
+
+
