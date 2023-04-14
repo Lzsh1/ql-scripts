5d6471354ab2653ed1ee852b490eedaf09ea5a8d
ZENG61 <74004927+ZENG61@users.noreply.github.com>
2023-04-14 09:07:30 +0800

    巴斯夫-最强安全大脑校园版

--- basf.py
+++ basf.py
@@ -1,34 +1,93 @@
 import requests
+import json
 
-url = 'https://gc-eassistance.basf.com/api/gear-campus-quiz/daily-exam/start'
+exam_questions_url = "https://gc-eassistance.basf.com/api/gear-campus-quiz/daily-exam/start"
+answer_url = "https://gc-eassistance.basf.com/api/gear-campus-quiz/daily-exam/answer"
+submit_url = "https://gc-eassistance.basf.com/api/gear-campus-quiz/daily-exam/complete"
 headers = {
-    'User-Agent': '',
-    'Cookie': ''
+    "Cookie": "SSESS14400d9688fc7430971ac15a078c0e5d=uzoQFu-xpo16gpr4N5m_99FYDuX6BHkyc4Wk8G1vHkU; openId=ozKsrs432_M3zIOyUnKZLUpUCcj0",
+    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.34(0x18002231) NetType/WIFI Language/zh_CN",
 }
 
-response = requests.post(url, headers=headers)##post考试的url
-data = response.json()##将响应主体放到data
+# 获取每日考试的问题ID和对应答案
+response = requests.get(exam_questions_url, headers=headers)
 
-if "questions" not in data:##如果考试url中不存在questions则代表考试已完成，有则获取考试的问题id
-    print("今日考试已完成")
-else:
-    id = data['questions']['id'][0]
-    answers = {}
-##将考试的问题id到每日学习的url进行查询答案，再输出答案
-    for question_id in question_ids:
-        data = {
-            "id": str(question_id),
+if response.status_code == 200:
+    data = json.loads(response.text)
+    questions = data['data']['questions']
+
+    # 用于存储问题ID和答案
+    question_answers = []
+    for question in questions:
+        question_id = question['id']
+        payload = {
+            "id": question_id,
             "userAnswer": [1]
         }
+        response = requests.post(answer_url, headers=headers, json=payload)
+        if response.status_code == 200:
+            answer_data = json.loads(response.text)
+            answer = answer_data['data']['correct']
+            question_answers.append({
+                "id": question_id,
+                "userAnswer": [answer]
+            })
+        else:
+            print("Error: ", response.status_code)
+else:
+    print("Error: ", response.status_code)
 
-        response = requests.post("https://gc-eassistance.basf.com/api/gear-campus-quiz/learning/answer", headers=headers, json=data)
-        answer = response.json()["data"]["answer"][0]
-        answers[str(question_id)] = answer
+# 构建新的请求主体
+payload = {
+    "userAnswers": [
+        {
+            "id": question_answers[0]['id'],
+            "userAnswer": [data1]
+        },
+        {
+            "id": question_answers[1]['id'],
+            "userAnswer": [data2]
+        },
+        {
+            "id": question_answers[2]['id'],
+            "userAnswer": [data3]
+        },
+        {
+            "id": question_answers[3]['id'],
+            "userAnswer": [data4]
+        },
+        {
+            "id": question_answers[4]['id'],
+            "userAnswer": [data5]
+        },
+        {
+            "id": question_answers[5]['id'],
+            "userAnswer": [data6]
+        },
+        {
+            "id": question_answers[6]['id'],
+            "userAnswer": [data7]
+        },
+        {
+            "id": question_answers[7]['id'],
+            "userAnswer": [data8]
+        },
+        {
+            "id": question_answers[8]['id'],
+            "userAnswer": [data9]
+        },
+        {
+            "id": question_answers[9]['id'],
+            "userAnswer": [data10]
+        }
+    ],
+    "examId": 42763
+}
 
-    print("问题的正确答案如下：")
+# 提交每日考试
+response = requests.post(submit_url, headers=headers, json=payload)
 
-    for question_id, answer in answers.items():
-        print(f"问题 '{question_id}' 的正确答案是: {answer}")
-
-
-
+if response.status_code == 200:
+    print("每日考试提交成功！")
+else:
+    print("每日考试提交失败。")
