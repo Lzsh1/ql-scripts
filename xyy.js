7e61a4249c4638737608e7a88241c2446a131d0b
Lzsh <74004927+Lzsh1@users.noreply.github.com>
2023-09-25 13:53:14 +0800

    Update xyy.js

--- xyy.js
+++ xyy.js
@@ -6,8 +6,7 @@
  */
 
 ck =$request.headers.Cookie
-var CookieValue = ck.match(/ysmuid=.+?;/)
-url = 'wskey'
+url = '小阅阅'
 console.log(ck)
-$notify(decodeURI(url), "小阅阅ck获取成功", "ysmuid="+CookieValue);
+$notify(decodeURI(url), "小阅阅ck获取成功", ck);
     $done();
